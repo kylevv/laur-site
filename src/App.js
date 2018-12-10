3d52f81af92be0102200220520f91daf769b70bb
Kyle Van Vleck <kylevanvleck@gmail.com>
2018-12-09 21:23:31 -0800

    Init React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './App.scss'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Laur Site</h1>
+        <p>Text 1</p>
+        <p>Text 2</p>
+      </div>
+    )
+  }
+}
+
+export default App

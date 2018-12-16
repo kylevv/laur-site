5e946ce504e12252c8df7438022f59e60cec822a
Kyle Van Vleck <kylevanvleck@gmail.com>
2018-12-15 23:21:59 -0800

    Get photos from S3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react'
+import AWS from 'aws-sdk'
 import './App.scss'
+const Bucket = 'laur-jewelry-photos'
+const baseUrl = 'https://s3.amazonaws.com'
+
+AWS.config.region = 'us-east-1'
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+  IdentityPoolId: 'us-east-1:4bf908fa-88f0-4793-a819-cf1e558097c3'
+})
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { photoKeys: [] }
+  }
+
+  componentDidMount () {
+    console.log('Hello World')
+    const s3 = new AWS.S3({
+      params: { Bucket }
+    })
+    s3.listObjects({}, (err, data) => {
+      if (err) {
+        console.log('ERR:', err)
+      } else {
+        console.log('DATA:', data)
+        this.setState((prevState) => {
+          const photoKeys = data.Contents.map((result) => result.Key)
+          return { photoKeys }
+        })
+      }
+    })
+  }
+
   render () {
     return (
       <div>
         <h1>Laur Site</h1>
         <p>Text 1</p>
         <p>Text 2</p>
+        {this.state.photoKeys.map((photoKey, i) =>
+          <img key={Date.now() + i} alt={photoKey} src={`${baseUrl}/${Bucket}/${photoKey}`} />
+        )}
       </div>
     )
   }
